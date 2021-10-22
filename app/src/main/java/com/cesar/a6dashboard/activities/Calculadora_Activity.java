5895b2925fa734a1612b2a809e45e16ca70f87c6
damA <cgorrizm@iesch.org>
2021-10-22 16:30:35 +0100

    hiden Action Bar

--- app/src/main/java/com/cesar/a6dashboard/activities/Calculadora_Activity.java
+++ app/src/main/java/com/cesar/a6dashboard/activities/Calculadora_Activity.java
@@ -12,5 +12,6 @@ public class Calculadora_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
+        getSupportActionBar().hide();
     }
 }
